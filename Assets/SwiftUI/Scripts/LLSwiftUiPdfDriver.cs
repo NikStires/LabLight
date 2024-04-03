a082321f997ae1b07dd5cd43929fba11394f647e
Nik Stires <nik.stires@gmail.com>
2024-04-03 12:53:49 -0700

    Moved swift pdf reader swift ui driver to the checklist panel, updated to open the pdf linked with the protocol

---
+++ Assets/SwiftUI/Scripts/LLSwiftUiPdfDriver.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using AOT;
+using PolySpatial.Samples;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class LLSwiftUiPdfDriver : MonoBehaviour
+{
+    [SerializeField]
+    UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable m_Button;
+
+    bool m_SwiftUIWindowOpen = false;
+
+    void OnEnable()
+    {
+        m_Button.selectEntered.AddListener(_ => TogglePDFWindow());
+    }
+
+    void OnDisable()
+    {
+        CloseSwiftUIWindow("PDF");
+    }
+
+    void Start()
+    {
+        if(string.IsNullOrEmpty(ProtocolState.procedureDef.pdfPath))
+        {
+            m_Button.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = Color.gray;
+            m_Button.enabled = false;
+        }
+    }
+
+    void TogglePDFWindow()
+    {
+        if (m_SwiftUIWindowOpen)
+        {
+            CloseSwiftUIWindow("PDF");
+            m_SwiftUIWindowOpen = false;
+        }
+        else
+        {
+            if(string.IsNullOrEmpty(ProtocolState.procedureDef.pdfPath))
+            {
+                return;
+            }
+            OpenSwiftPdfWindow(Path.GetFileNameWithoutExtension(ProtocolState.procedureDef.pdfPath));
+            m_SwiftUIWindowOpen = true;
+        }
+    }
+
+    #if UNITY_VISIONOS && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    static extern void OpenSwiftPdfWindow(string pdfUrlString);
+    [DllImport("__Internal")]
+    static extern void CloseSwiftUIWindow(string name);
+    #else
+    static void OpenSwiftPdfWindow(string pdfUrlString) {}
+    static void CloseSwiftUIWindow(string name) {}
+    #endif
+}
