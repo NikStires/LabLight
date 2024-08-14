e12963bd784215bebf404f1a472ade65a2753024
Nik Stires <nik.stires@gmail.com>
2024-08-14 15:31:29 -0700

    Updated wellplate settings buttons with new icons and animations

--- Assets/_App/Scripts/UI/DisableOutlineButton.cs
+++ Assets/_App/Scripts/UI/DisableOutlineButton.cs
@@ -1,26 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class DisableOutlineButton : MonoBehaviour
 {
-    [SerializeField]
-    public PlaneInteractionManagerScriptableObject planeInteractionManagerSO;
-    private XRSimpleInteractable interactable;
-
     public GameObject outline;
 
+    [SerializeField] private Material toggleOffMaterial;
+    [SerializeField] private Material toggleOnMaterial;
+    private MeshRenderer meshRenderer;
+
+    [SerializeField] Sprite toggleOnSprite;
+    [SerializeField] Sprite toggleOffSprite;
+    [SerializeField] Image image;
 
     public void Start()
     {
-        interactable = GetComponent<XRSimpleInteractable>();
-        interactable.selectEntered.AddListener(_ => toggleOutline());
-    }
-
-    public void OnDestroy()
-    {
-        interactable.selectEntered.RemoveListener(_ => toggleOutline());
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
 
@@ -28,5 +26,15 @@ public class DisableOutlineButton : MonoBehaviour
     public void toggleOutline()
     {
         outline.SetActive(!outline.activeSelf);
+        if(outline.activeSelf)
+        {
+            meshRenderer.material = toggleOnMaterial;
+            image.sprite = toggleOnSprite;
+        }
+        else
+        {
+            meshRenderer.material = toggleOffMaterial;
+            image.sprite = toggleOffSprite;
+        }
     }
 }
