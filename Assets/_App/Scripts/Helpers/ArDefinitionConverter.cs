06ab26ffb3431daa17b70d822d23d69012a68a29
GeorgeZipp <aliensareblue@gmail.com>
2024-11-17 15:11:58 -0800

    commented out old converters

--- Assets/_App/Scripts/Helpers/ArDefinitionConverter.cs
+++ Assets/_App/Scripts/Helpers/ArDefinitionConverter.cs
@@ -1,68 +1,68 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
-using System;
-using UnityEngine;
+// using Newtonsoft.Json;
+// using Newtonsoft.Json.Converters;
+// using Newtonsoft.Json.Linq;
+// using System;
+// using UnityEngine;
 
-/// <summary>
-/// Json conversion helper to create concrete ArDefinition classes
-/// </summary>
-public class ArDefinitionConverter : CustomCreationConverter<ArDefinition>
-{
-    const string refProperty = "$ref";
-    const string arDefinitionTypeProperty = "arDefinitionType";
+// /// <summary>
+// /// Json conversion helper to create concrete ArDefinition classes
+// /// </summary>
+// public class ArDefinitionConverter : CustomCreationConverter<ArDefinition>
+// {
+//     const string refProperty = "$ref";
+//     const string arDefinitionTypeProperty = "arDefinitionType";
 
-    // Deprecated property name, use arDefinitionTypeProperty
-    const string actionProperty = "action";
+//     // Deprecated property name, use arDefinitionTypeProperty
+//     const string actionProperty = "action";
 
-    ArDefinitionType _currentObjectType;
+//     ArDefinitionType _currentObjectType;
 
-    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    {
-        var jobj = JObject.ReadFrom(reader);
+//     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+//     {
+//         var jobj = JObject.ReadFrom(reader);
 
-        var refId = (string)jobj[refProperty].RemoveFromLowestPossibleParent();
-        if (refId != null)
-        {
-            var reference = serializer.ReferenceResolver.ResolveReference(serializer, refId);
-            if (reference != null)
-                return reference;
-        }
+//         var refId = (string)jobj[refProperty].RemoveFromLowestPossibleParent();
+//         if (refId != null)
+//         {
+//             var reference = serializer.ReferenceResolver.ResolveReference(serializer, refId);
+//             if (reference != null)
+//                 return reference;
+//         }
 
-        if (jobj[arDefinitionTypeProperty] != null)
-        {
-            _currentObjectType = jobj[arDefinitionTypeProperty].ToObject<ArDefinitionType>();
-        }
-        else
-        {
-            _currentObjectType = jobj["action"].ToObject<ArDefinitionType>();
-        }
+//         if (jobj[arDefinitionTypeProperty] != null)
+//         {
+//             _currentObjectType = jobj[arDefinitionTypeProperty].ToObject<ArDefinitionType>();
+//         }
+//         else
+//         {
+//             _currentObjectType = jobj["action"].ToObject<ArDefinitionType>();
+//         }
 
-        return base.ReadJson(jobj.CreateReader(), objectType, existingValue, serializer);
-    }
+//         return base.ReadJson(jobj.CreateReader(), objectType, existingValue, serializer);
+//     }
     
-    public override ArDefinition Create(Type objectType)
-    {
-        switch (_currentObjectType)
-        {
-            case ArDefinitionType.Line:
-                return new LineArDefinition();
-            case ArDefinitionType.Model:
-                return new ModelArDefinition();
-            case ArDefinitionType.Outline:
-                return new OutlineArDefinition();
-            case ArDefinitionType.Overlay:
-                return new OverlayArDefinition();
-            case ArDefinitionType.Container:
-                return new ContainerArDefinition();
-            case ArDefinitionType.Mask:
-                return new MaskArDefinition();
-            case ArDefinitionType.Arrow:
-                return new ArrowArDefinition();
-            case ArDefinitionType.BoundingBox:
-                return new BoundingBoxArDefinition();
-            default:
-                throw new NotImplementedException();
-        }
-    }
-}
+//     public override ArDefinition Create(Type objectType)
+//     {
+//         switch (_currentObjectType)
+//         {
+//             case ArDefinitionType.Line:
+//                 return new LineArDefinition();
+//             case ArDefinitionType.Model:
+//                 return new ModelArDefinition();
+//             case ArDefinitionType.Outline:
+//                 return new OutlineArDefinition();
+//             case ArDefinitionType.Overlay:
+//                 return new OverlayArDefinition();
+//             case ArDefinitionType.Container:
+//                 return new ContainerArDefinition();
+//             case ArDefinitionType.Mask:
+//                 return new MaskArDefinition();
+//             case ArDefinitionType.Arrow:
+//                 return new ArrowArDefinition();
+//             case ArDefinitionType.BoundingBox:
+//                 return new BoundingBoxArDefinition();
+//             default:
+//                 throw new NotImplementedException();
+//         }
+//     }
+// }
--- Assets/_App/Scripts/Helpers/ArOperationConverter.cs
+++ Assets/_App/Scripts/Helpers/ArOperationConverter.cs
@@ -1,36 +1,36 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
-using System;
+// using Newtonsoft.Json;
+// using Newtonsoft.Json.Converters;
+// using Newtonsoft.Json.Linq;
+// using System;
 
-/// <summary>
-/// Json conversion helper to create concrete ArOperation classes
-/// </summary>
-public class ArOperationConverter : CustomCreationConverter<ArOperation>
-{
-    ArOperationType _currentObjectType;
+// /// <summary>
+// /// Json conversion helper to create concrete ArOperation classes
+// /// </summary>
+// public class ArOperationConverter : CustomCreationConverter<ArOperation>
+// {
+//     ArOperationType _currentObjectType;
 
-    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    {
-        var jobj = JObject.ReadFrom(reader);
+//     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+//     {
+//         var jobj = JObject.ReadFrom(reader);
 
-        _currentObjectType = jobj["arOperationType"].ToObject<ArOperationType>();
+//         _currentObjectType = jobj["arOperationType"].ToObject<ArOperationType>();
  
-        return base.ReadJson(jobj.CreateReader(), objectType, existingValue, serializer);
-    }
+//         return base.ReadJson(jobj.CreateReader(), objectType, existingValue, serializer);
+//     }
     
-    public override ArOperation Create(Type objectType)
-    {
-        switch (_currentObjectType)
-        {
-            case ArOperationType.Highlight:
-                return new HighlightArOperation();
-            //case ArOperationType.Animation:
-            //    return new AnimationArOperation();
-            case ArOperationType.Anchor:
-                return new AnchorArOperation();
-            default:
-                throw new NotImplementedException();
-        }
-    }
-}
+//     public override ArOperation Create(Type objectType)
+//     {
+//         switch (_currentObjectType)
+//         {
+//             case ArOperationType.Highlight:
+//                 return new HighlightArOperation();
+//             //case ArOperationType.Animation:
+//             //    return new AnimationArOperation();
+//             case ArOperationType.Anchor:
+//                 return new AnchorArOperation();
+//             default:
+//                 throw new NotImplementedException();
+//         }
+//     }
+// }
