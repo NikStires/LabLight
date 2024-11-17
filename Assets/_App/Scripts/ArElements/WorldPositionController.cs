d2ae526ec51b4cab5100b1754375ee410ca98b95
GeorgeZipp <aliensareblue@gmail.com>
2024-11-16 19:38:53 -0800

    Update WorldPositionController.cs

--- Assets/_App/Scripts/ArElements/WorldPositionController.cs
+++ Assets/_App/Scripts/ArElements/WorldPositionController.cs
@@ -62,6 +62,7 @@ public class WorldPositionController : ArElementViewController
                 // {
                 //     transform.position = ((ModelArObject)this.arObject).position;
                 // }
+                }
             }
         }
         else
